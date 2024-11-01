d78762fa4a24b03508ac8279ec77ed0ea1394752
Ludovic Mermod <ludovic.mermod@epfl.ch>
2024-11-01 22:48:17 +0100

    feat: add all database functions

--- src/data.ts
+++ src/data.ts
@@ -8,8 +8,9 @@ import { existsSync, readFileSync } from "node:fs";
 export interface Command {
   id: number;
   amounts: number[];
+  register: string;
   prepared: boolean;
-  given: boolean;
+  served: boolean;
 }
 
 var lock = new Mutex();
@@ -21,13 +22,14 @@ if (existsSync("db.json")) {
   commands = JSON.parse(readFileSync("db.json").toString());
 }
 
-export async function addCommand(amounts: number[]) {
+export async function addCommand(amounts: number[], register: string) {
   await lock.runExclusive(async () => {
     commands.push({
       id: seq,
       amounts,
+      register,
       prepared: false,
-      given: false,
+      served: false,
     });
 
     seq += 1;
@@ -38,6 +40,36 @@ export async function addCommand(amounts: number[]) {
 
 export async function getCommands(): Promise<Command[]> {
   return commands;
+}
+
+export async function getCommandToPrepare(): Promise<Command[]> {
+  return commands.filter((c) => c.prepared);
+}
+
+export async function getCommandToServe(register: string): Promise<Command[]> {
+  return commands.filter(
+    (c) => c.prepared && !c.served && c.register == register
+  );
+}
+
+export async function markAsPrepared(id: number) {
+  await lock.runExclusive(async () => {
+    const command = commands.find((c) => c.id == id);
+    if (command) {
+      command.prepared = true;
+      await save();
+    }
+  });
+}
+
+export async function markAsServed(id: number) {
+  await lock.runExclusive(async () => {
+    const command = commands.find((c) => c.id == id);
+    if (command) {
+      command.served = true;
+      await save();
+    }
+  });
 }
 
 async function save() {
