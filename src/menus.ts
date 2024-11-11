258ac5b5d3a174a5b931ce83732d29eab3fc83b0
Sidonie Bouthors <sidonie.bouthors@epfl.ch>
2024-11-11 17:12:45 +0100

    feat: add initial stock to menu

--- src/menus.ts
+++ src/menus.ts
@@ -1,11 +1,12 @@
 export interface Menu {
   name: string;
   description: string;
+  initialStock: number;
 }
 
 export const MENUS: Menu[] = [
-  { name: "Temptation", description: "Tomates séchées, Mozza, Pesto" },
-  { name: "Chaos", description: "Chèvre, Miel" },
-  { name: "Oblivion", description: "Champignons, Oignons caramélisés, Pesto" },
-  { name: "Craving", description: "Ovomaltine" },
+  { name: "Temptation", description: "Tomates séchées, Mozza, Pesto", initialStock: 10 },
+  { name: "Chaos", description: "Chèvre, Miel", initialStock: 10 },
+  { name: "Oblivion", description: "Champignons, Oignons caramélisés, Pesto", initialStock: 10 },
+  { name: "Craving", description: "Ovomaltine", initialStock: 10 },
 ];
