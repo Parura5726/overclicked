5280eb980ed9f887d4834236b054bd8355e81ee0
Sidonie Bouthors <sidonie.bouthors@epfl.ch>
2024-11-11 17:13:20 +0100

    feat: add admin panel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,126 @@
+"use client";
+
+import OrderStatus from "@/components/OrderStatus";
+import { getOrders, markAsPrepared, markAsServed, Order } from "@/data";
+import { MENUS } from "@/menus";
+import { delay } from "@/utils";
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const [orders, setOrders] = useState([] as Order[]);
+
+  useEffect(() => {
+    let fetch = true;
+    let fn = async () => {
+      while (fetch) {
+        setOrders(await getOrders());
+        await delay(1000);
+      }
+    };
+
+    fn();
+
+    return () => {
+      fetch = false;
+    };
+  }, []);
+
   return (
-    <div>
+    <div className="admin">
       <h1>Subsonic Catering</h1>
+      <div className="stats">
+        <div className="stat">
+          <h2>Total</h2>
+          <p>{orders.length} Orders</p>
+          <p>
+            {orders.reduce((acc, o) => {
+              return acc + o.amounts.reduce((acc, a) => acc + a, 0);
+            }, 0)}{" "}
+            Croques
+          </p>
+        </div>
+        <div className="stat">
+          <h2>Prepared</h2>
+          <p>{orders.filter((o) => o.prepared).length} Orders</p>
+          <p>
+            {orders
+              .filter((o) => o.prepared)
+              .reduce((acc, o) => {
+                return acc + o.amounts.reduce((acc, a) => acc + a, 0);
+              }, 0)}{" "}
+            Croques
+          </p>
+        </div>
+        <div className="stat">
+          <h2>Served</h2>
+          <p>{orders.filter((o) => o.served).length} Orders</p>
+          <p>
+            {orders
+              .filter((o) => o.served)
+              .reduce((acc, o) => {
+                return acc + o.amounts.reduce((acc, a) => acc + a, 0);
+              }, 0)}{" "}
+            Croques
+          </p>
+        </div>
+      </div>
+      <div className="stats">
+        {MENUS.map((m, i) => (
+          <div key={i} className="stat">
+            <h2>{m.name}</h2>
+            <p>{orders.reduce((acc, o) => acc + o.amounts[i], 0)} Ordered</p>
+            <p>
+              {m.initialStock -
+                orders.reduce((acc, o) => acc + o.amounts[i], 0)}{" "}
+              Remaining
+            </p>
+          </div>
+        ))}
+      </div>
+      <div className="stats">
+        {orders
+          .reduce((acc, o) => {
+            if (!acc.includes(o.register)) {
+              acc.push(o.register);
+            }
+            return acc;
+          }, [] as string[])
+          .map((r, i) => (
+            <div key={i} className="stat">
+              <h2>Register {r}</h2>
+              <p>{orders.filter((o) => o.register === r).length} Orders</p>{" "}
+              <p>
+                {orders
+                  .filter((o) => o.register === r)
+                  .reduce((acc, o) => {
+                    return acc + o.amounts.reduce((acc, a) => acc + a, 0);
+                  }, 0)}{" "}
+                Croques Sold
+              </p>
+            </div>
+          ))}
+      </div>
+      <div className="orders">
+        {orders
+          .sort((a, b) => b.id - a.id)
+          .map((o) => (
+            <OrderStatus
+              key={o.id}
+              menuItems={MENUS}
+              order={o}
+              showDetails={false}
+              showRegister={true}
+              archived={o.served}
+              statusIcon={o.served ? "✅" : o.prepared ? "🥪" : "⏳"}
+              actionAvailable={!o.served || !o.prepared}
+              buttonText={"Prepared & Served"}
+              buttonAction={() => {
+                markAsPrepared(o.id);
+                markAsServed(o.id);
+              }}
+            />
+          ))}
+      </div>
     </div>
-  )
+  );
 }
