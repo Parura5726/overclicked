3a6ed5ac7ac6bfa6cbdf8102087202609cd4ecab
Sidonie Bouthors <sidonie.bouthors@epfl.ch>
2024-11-12 11:00:53 +0100

    fix: use appropriate orders to serve function

--- src/app/[register]/public/page.tsx
+++ src/app/[register]/public/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { getOrders, Order } from "@/data";
+import { getOrders, getOrdersToServe, Order } from "@/data";
 import { MENUS } from "@/menus";
 import { delay } from "@/utils";
 import { useParams } from "next/navigation";
@@ -9,13 +9,13 @@ import { useEffect, useState } from "react";
 export default function Public() {
   const params = useParams();
   const register = params["register"] as string;
-  const [orders, setOrders] = useState([] as Order[]);
+  const [ordersToServe, setOrdersToServe] = useState([] as Order[]);
 
   useEffect(() => {
     let fetch = true;
     let fn = async () => {
       while (fetch) {
-        setOrders(await getOrders());
+        setOrdersToServe(await getOrdersToServe(register));
         await delay(1000);
       }
     };
@@ -26,14 +26,6 @@ export default function Public() {
       fetch = false;
     };
   }, []);
-
-  let pickupOrders = orders
-    .filter((o) => o.register === register && o.prepared && !o.served)
-    .map((o) => (
-      <div key={o.id} className="order">
-        <h2>Order {o.id}</h2>
-      </div>
-    ));
 
   return (
     <div className="public">
@@ -49,8 +41,12 @@ export default function Public() {
       </div>
       <div className="ready-orders">
         <h1>Ready for Pick Up</h1>
-        {pickupOrders.length > 0 ? (
-          pickupOrders.slice(0, 4)
+        {ordersToServe.length > 0 ? (
+          ordersToServe.slice(0, 4).map((o) => (
+            <div key={o.id} className="order">
+              <h2>Order {o.id}</h2>
+            </div>
+          ))
         ) : (
           <p>No orders ready : please wait !</p>
         )}
